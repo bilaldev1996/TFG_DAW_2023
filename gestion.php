50c6a5b69f9ae2753e8d6b68c3d35b51b39cacf0
Bilal al messaoui <100048942+bilaldev1996@users.noreply.github.com>
2023-05-26 10:25:01 +0200

    Mejorando correo

---
+++ gestion.php
@@ -0,0 +1,260 @@
+<?php
+
+class gestion
+{
+    public $view;
+    public $gestionar;
+    public $title;
+
+    public $centro;
+    public $empresa;
+
+
+    public function __construct()
+    {
+        $this->view = 'Login';
+        $this->gestionar = new Gestionar();
+        $this->centro = new CentroEducativo(NULL, NULL, NULL, NULL, NULL, NULL);
+    }
+
+    public function login()
+    {
+        if (isset($_SESSION['admin'])) {
+            return $this->verPanel();
+        } else {
+            $this->view = 'login';
+            $this->title = 'Inicio de Sesión';
+        }
+    }
+
+    public function logearAdmin()
+    {
+        $password = $_POST['password'] ?? '';
+        $email = $_POST['email'] ?? '';
+
+        if (!empty($password) && !empty($email)) {
+            $logeo = $this->gestionar->login($password, $email);
+
+            if ($logeo) {
+                $this->verPanel();
+                header('Location: index.php');
+                return $logeo;
+            }
+        }
+
+        $this->login();
+        return false;
+    }
+
+
+    /* Redirigir al dashboard si cumple la condición, devolver los número de registros en un array */
+    public function verPanel()
+    {
+        if (isset($_SESSION['admin'])) {
+            $this->view = 'dashboard';
+            $this->title = 'Dashboard';
+            $numero_estudiantes = $this->gestionar->getNumeroRegistros('estudiante');
+            $numero_empresas = $this->gestionar->getNumeroRegistros('empresa');
+            $numero_centros = $this->gestionar->getNumeroRegistros('centroeducativo');
+            $numero_ofertas = $this->gestionar->getNumeroRegistros('ofertaempleo');
+            return [$numero_estudiantes, $numero_empresas, $numero_centros, $numero_ofertas];
+        } else {
+            $this->login();
+        }
+    }
+
+    public function logout()
+    {
+        $this->gestionar->close();
+        $this->login();
+    }
+
+
+    /* Enviar email para restablecer contraseña */
+    public function enviarEmail()
+    {
+        $email = $_POST['email'];
+
+        if ($email !== 'administrador@jobsnow.es') {
+            $this->view = 'login';
+            return;
+        };
+
+        $asunto = 'Restablecer Password';
+        $cuerpo = '
+            <h2>Recuperar contraseña</h2>
+            <p>Has solicitado recuperar tu contraseña. Por favor haz clic en el siguiente enlace para restablecerla:</p>
+            <a href="http://jobsnow.es/administrador/index.php?action=restablecerContraseña">Restablecer contraseña</a>
+            <p>Si no solicitaste recuperar tu contraseña, por favor ignora este mensaje.</p>
+        ';
+        // Headers del correo
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+        $headers .= "From: JobsNow <" . 'administrador@jobsnow.es' . ">\r\n";
+        $headers .= "Reply-To: " . $email . "\r\n";
+
+        // Enviamos el correo usando la función mail()
+        if (mail($email, $asunto, $cuerpo, $headers)) {
+            $this->view = 'login';
+            return 'enviado';
+        }
+    }
+
+
+    public function restablecerContraseña()
+    {
+        $this->view = 'restablecerContraseña';
+        $this->title = 'Restablecer Contraseña';
+    }
+
+    /* Función que restablece la contraseña */
+    public function cambiarContraseña()
+    {
+        $pass = $_POST['password'];
+        $pass_confirm = $_POST['password-confirm'];
+
+        if (empty($pass) || empty($pass_confirm)) {
+            return false;
+        }
+
+        if ($pass !== $pass_confirm) {
+            //echo "Las contraseñas no coinciden";
+            $this->view = 'restablecerContraseña';
+            return false;
+        }
+
+        if (!$this->gestionar->cambiarContraseña($pass)) {
+            $this->view = 'restablecerContraseña';
+            return false;
+        }
+
+        $this->view = 'login';
+
+
+        return 'contraseña cambiada';
+    }
+
+    public function gestionarEstudiantes()
+    {
+        $this->view = 'gestionarEstudiantes';
+        return $this->gestionar->getEstudiantes();
+    }
+
+    public function gestionarEmpresas()
+    {
+        $this->view = 'gestionarEmpresas';
+        return $this->gestionar->getEmpresas();
+    }
+
+    public function gestionarCentros()
+    {
+        $this->view = 'gestionarCentros';
+        return $this->gestionar->getCentrosEducativos();
+    }
+
+    public function gestionarOfertas()
+    {
+        $this->view = 'gestionarOfertas';
+        return $this->gestionar->getOfertas();
+    }
+
+    public function eliminarEstudiante()
+    {
+        if (isset($_GET['id'])) {
+            $idEstudiante = $_GET['id'];
+            $tabla = 'estudiante';
+            $campo = 'idEstudiante';
+            $this->gestionar->eliminarRegistro($idEstudiante, $tabla, $campo);
+        }
+    }
+
+    public function eliminarEmpresa()
+    {
+        if (isset($_GET['id'])) {
+            $idEmpresa = $_GET['id'];
+            $tabla = 'empresa';
+            $campo = 'idEmpresa';
+            $this->gestionar->eliminarRegistro($idEmpresa, $tabla, $campo);
+        }
+    }
+
+    public function eliminarCentro()
+    {
+        if (isset($_GET['id'])) {
+            $idCentro = $_GET['id'];
+            $tabla = 'centroeducativo';
+            $campo = 'idCentro';
+            $this->gestionar->eliminarRegistro($idCentro, $tabla, $campo);
+        }
+    }
+
+    public function eliminarOferta()
+    {
+        if (isset($_GET['id'])) {
+            $idOferta = $_GET['id'];
+            $tabla = 'ofertaempleo';
+            $campo = 'idOferta';
+            $this->gestionar->eliminarRegistro($idOferta, $tabla, $campo);
+        }
+    }
+
+    /* Dar de alta a un centro educativo */
+    public function altaCentro()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            // Recoger los datos del formulario
+            $nombre = $_POST["nombre"];
+            $direccion = $_POST["direccion"];
+            $telefono = $_POST["telefono"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+
+            //comprobar si el email existe
+            if (!$this->centro->comprobarEmail($_POST['email'])) {
+                $this->gestionarCentros();
+                return 'email-existe';
+            };
+
+            $this->centro->altaCentro($nombre, $direccion, $telefono, $email, $password);
+
+
+            $this->gestionarCentros();
+            return "centro-añadido";
+        }
+    }
+
+    /* Validar empresa y enviar email si ha sido verificada */
+
+    public function verificarEmpresa()
+    {
+        $idEmpresa = $_GET['idEmpresa'];
+        $email = $_GET['email'];
+        $nombre = $_GET['nombre'];
+
+        $this->gestionar->verificarEmpresa($idEmpresa, 1);
+
+        $asunto = 'Cuenta Validada';
+
+
+        $cuerpo = '<p>Estimado equipo de' . $nombre . '</p>
+        <p>¡Nos complace informarles que su cuenta ha sido validada exitosamente!</p>
+        <p>En nombre de nuestro equipo, queremos felicitarles por completar exitosamente el proceso de validación de su cuenta en nuestra plataforma. Esto significa que ahora tienen acceso completo a todas las funcionalidades y características de nuestra plataforma JobsNow.</p>
+        <p>Para comenzar a utilizar su cuenta, por favor, haga clic en el siguiente enlace para iniciar sesión:</p>
+        <p><a href="https://jobsnow.es/index.php?controller=EmpresaController&action=accesoEmpresa" class="text-blue-500">Iniciar Sesión</a></p>
+        <p>Gracias por confiar en nosotros y por formar parte de nuestra comunidad. Estamos comprometidos en proporcionarles la mejor experiencia posible y en apoyarles en su búsqueda de talento o cualquier otro objetivo que tengan en mente.</p>
+        <p>Si tienen alguna pregunta, inquietud o necesitan asistencia adicional, no duden en ponerse en contacto con nuestro equipo.</p>';
+
+        // Headers del correo
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+        $headers .= "From: JobsNow <" . 'administrador@jobsnow.es' . ">\r\n";
+        $headers .= "Reply-To: " . $email . "\r\n";
+
+        // Enviamos el correo usando la función mail()
+        if (mail($email, $asunto, $cuerpo, $headers)) {
+            return $this->gestionarEmpresas();
+        }
+    }
+}
---
+++ gestionarEmpresas.php
@@ -0,0 +1,53 @@
+<main class="flex flex-col lg:flex-row h-screen bg-gray-100">
+    <?php include 'template/navbar.php' ?>
+
+    <!-- Contenido -->
+    <section class="w-full  px-0 lg:px-3 mt-1">
+
+        <div class="bg-white p-6 rounded-lg shadow">
+            <div class="flex justify-between">
+                <h3 class="font-bold text-xl mb-3">Gestionar Empresas</h3>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2">Nombre</th>
+                            <th class="px-4 py-2">Email</th>
+                            <th class="px-4 py-2">CIF</th>
+                            <th class="px-4 py-2">Verificada</th>
+                            <th class="px-4 py-2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (!empty($dataToView)) : ?>
+                            <?php foreach ($dataToView as $empresa) : ?>
+                                <tr>
+                                    <td class="border px-4 py-2"><?= $empresa->getNombre() ?></td>
+                                    <td class="border px-4 py-2"><?= $empresa->getEmail() ?></td>
+                                    <td class="border px-4 py-2"><?= $empresa->getCIF() ?></td>
+                                    <?php if ($empresa->getVerificado() == 1) : ?>
+                                        <td class="border px-4 py-2">Sí</td>
+                                    <?php else : ?>
+                                        <td class="border px-4 py-2">No</td>
+                                    <?php endif ?>
+
+                                    <td class="border px-4 py-2 flex gap-2">
+                                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="confirmarEliminar(<?php echo $empresa->getIdEmpresa() ?>,'eliminarEmpresa')">Eliminar</button>
+                                        <?php if ($empresa->getVerificado() == 0) : ?>
+                                            <a href="index.php?controller=gestion&action=verificarEmpresa&idEmpresa=<?= $empresa->getIdEmpresa() ?>&email=<?= $empresa->getEmail() ?>&nombre=<?= $empresa->getNombre() ?>" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Verificar</a>
+                                        <?php endif ?>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td class="border px-4 py-2" colspan="4">No se encontraron resultados</td>952349790
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</main>
